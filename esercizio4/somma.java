b513e0ead325ed376da0e90aae9050ddfc66ac77
AndreaAlvigini <andreaalvigini@gmail.com>
2023-05-16 12:32:53 +0200

    somme

---
+++ esercizio4/somma.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//package Esercizio_replica.esercizio4;
+
+public class somma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("inserisci il primo numero");
+        int a= input.nextInt();
+        System.out.println("inserisci il secondo numero");
+        int b= input.nextInt();
+        System.out.println("la somma è: "+ (a+b));
+
+    }
+    
+}
---
+++ esercizio5/AreaPerimetro.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//package Esercizio_replica.esercizio5;
+
+public class AreaPerimetro {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("inserisci il lato");
+        double lato = input.nextDouble();
+        double perimetro = lato*4;
+        double area = lato*lato;
+
+        if(lato>40){
+            System.out.println("il lato è troppo grande");
+        }
+        if(lato<0){
+            System.out.println("il lato è troppo piccolo");
+        }
+        else{
+           
+
+        System.out.println("il perimetro è "+ perimetro);
+        System.out.println("l'area è "+ area);
+
+    }
+}
+}
